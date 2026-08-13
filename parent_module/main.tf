439b85042fd14190a9317a090c270f7451bda197
dupadhayay <deepak.upadhayay@telusinternational.com>
2026-08-13 23:30:58 +0530

    fix keyvault issue

--- parent_module/main.tf
+++ parent_module/main.tf
@@ -6,11 +6,11 @@ output "rg_names" {
   value = module.resource.resource_group_names
 }
 
-module "keyVault" {
-  depends_on = [module.resource]
-  source     = "./child_module/azurerm_keyvalult"
-  keyvault   = var.keyvault
-}
+# module "keyVault" {
+#   depends_on = [module.resource]
+#   source     = "./child_module/azurerm_keyvalult"
+#   keyvault   = var.keyvault
+# }
 
 module "public_ip" {
   depends_on = [module.resource]
@@ -31,7 +31,7 @@ module "subnet" {
 }
 
 module "VM" {
-  depends_on = [module.resource, module.public_ip, module.subnet, module.vnet, module.keyVault]
+  depends_on = [module.resource, module.public_ip, module.subnet, module.vnet]
 
   source = "./child_module/azurerm_virtual_machine"
   VM     = var.vms
